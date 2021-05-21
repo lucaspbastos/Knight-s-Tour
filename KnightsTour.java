a73f53b07eee7812b89dd942d2020dcdf2981561
Lucas Bastos <lucas@lucasbastos.com>
2021-05-21 13:48:50 -0400

    Initialize project

---
+++ KnightsTour.java
@@ -0,0 +1,53 @@
+/*
+    Lucas Bastos
+    CS 114-H01
+*/
+public class KnightsTour {
+    // Initialize board
+    private static int m = 8;
+    private static int n = 8;
+    private static int[][] board = new int[m][n];
+
+	public static void main(String[] args) {
+		move(0,0,0);
+		steps();
+	}	
+
+    // Print steps
+	public static void steps(){
+		for(int i = 0; i < m; i++){
+			for(int j = 0; j < n; j++){
+				if (board[i][j] >= 10 || board[i][j] == -1){
+					System.out.print(board[i][j] + " ");
+                } else {
+                    System.out.print(board[i][j] + "  ");
+                }
+			}
+			System.out.println();
+		}	
+    }
+
+    // Check move validity
+    public static boolean isValid(int row, int col) {
+		return (row <= m-1 && col <= n-1 && row >= 0 && col >= 0 && (board[row][col] == 0));
+    }
+
+    // Recursively discover moves
+	public static boolean move(int row, int col, int step){
+		board[row][col] = step;
+        if (step>=(m*n)-1
+        ||(isValid(row+2,col+1) && move(row+2, col+1, step+1))
+        ||(isValid(row+1,col+2) && move(row+1, col+2, step+1))
+        ||(isValid(row-1,col+2) && move(row-1, col+2, step+1))
+        ||(isValid(row-2,col+1) && move(row-2, col+1, step+1))
+        ||(isValid(row-2,col-1) && move(row-2, col-1, step+1))
+        ||(isValid(row-1,col-2) && move(row-1, col-2, step+1))
+        ||(isValid(row+1,col-2) && move(row+1, col-2, step+1))
+        ||(isValid(row+2,col-1) && move(row+2, col-1, step+1))){
+            return true;
+        } else{
+            board[row][col] = 0;
+            return false;
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# CS 114 Knight's Tour
+This project implements the [Knight's Tour](https://en.wikipedia.org/wiki/Knight's_tour) problem recursively in Java for Fall 2019 CS 114 taught by Professor Kapleau. This is for archival purposes and should not be used for any students currently taking CS 114.
+
+## Setup
+The program can be ran by first compiling the Java program with `javac KnightsTour.java` and then running with `java KnightsTour`. The program is set with _m_ = _n_ = 8, but _m_ and _n_ can be changed in lines 7 and 8.
